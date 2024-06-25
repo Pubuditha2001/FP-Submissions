e1efdd7046493116f4a8c0d56d5315c4cf559c40
Pubuditha <140165090+Pubuditha2001@users.noreply.github.com>
2024-06-25 09:43:25 +0530

    Update 2.scala

--- 2.scala
+++ 2.scala
@@ -5,10 +5,10 @@ object javatoscala{
         var k: Float = 4.3f
 
         //if in c
-        val result1 = (b-1) * a + c * d
+        val result1 = (b-1) * a + c * (d-1)
         println( "--b * a + c * d-- = " +result1);
 
-        val result2 = a
+        val result2 = a+1
         println("a++ = " +result2)
 
         //dont konw the "g" in (d - k)
@@ -16,10 +16,10 @@ object javatoscala{
         println("-2 * ( g - k ) +c = " +result3)
 
         c=c
-        val result4 = c
+        val result4 = c+1
         println("c=c++ = " +result4)
 
-        c=(c+1)*a
+        c=(c+1)*(a+1)
         val result5 = c
         println("c=++c*a++ = " +result5);
 
