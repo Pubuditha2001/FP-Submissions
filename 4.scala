81aa28c290bac472cb53a8a9aed7e61420c75bfc
Pubuditha <140165090+Pubuditha2001@users.noreply.github.com>
2024-06-25 05:41:32 +0530

    Update 4.scala

--- 4.scala
+++ 4.scala
@@ -1,45 +1,16 @@
 object Theater{
+    def main(args: Array[String]): Unit = {
 
+        val basePrice = 15.0
+        val initialAttendance = 120
+        val costPerPerformance = 500
+        val costPerAttendee = 3.0
+
+        val priceDecrease = 5.0
+        val attendanceIncrease = 20
+        
+  }
+}
     
 
-    def main(args: Array[String]): Unit={
-
-        
-
-    }
-
-    def normalpay():Int ={
-        val normal_hours_pay = pay_per_working*w_hours
-        println("Normal hours payment per week = " +normal_hours_pay)
-        normal_hours_pay
-    }
-
-    def ot_pay():Int ={
-        val ot_hours_pay = pay_per_ot*ot_hours
-        println("OT hours payment per week = " +ot_hours_pay)
-        ot_hours_pay
-    }
-
-    def fullpaymentPerWeek():Int ={
-        val totalpayment = normalpay() + ot_pay()
-        println("Total payment per week = " +totalpayment)
-        totalpayment
-    }
-
-    def totalpaymentpermonth():Double ={
-        val totalpayment_month = fullpaymentPerWeek()/7*30
-        println("Total payment of month = " +totalpayment_month)
-        totalpayment_month
-    }
-   
-    def totaltax():Double ={
-        val tax= totalpaymentpermonth()*tax_per
-        println("Total tax = " +tax)
-        tax
-    }
-
-    def takehomesalaty():Unit = {
-        val result = totalpaymentpermonth() - totaltax()
-        println("Take Home Salary = " +result)
-    }
-}
+    
